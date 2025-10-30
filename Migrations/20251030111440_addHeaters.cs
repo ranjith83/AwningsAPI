9c8d5bd8a06a2d7c45eedbe9071b3bfa2680441e
gistyf30-dev <gistyf30@hotmail.com>
2025-10-30 11:23:51 +0000

    added heaters

---
+++ Migrations/20251030111440_addHeaters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AwningsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addHeaters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heaters",
+                columns: table => new
+                {
+                    HeaterId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    PriceNonRALColour = table.Column<decimal>(type: "decimal(18,4)", precision: 18, scale: 4, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heaters", x => x.HeaterId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Heaters",
+                columns: new[] { "HeaterId", "CreatedBy", "DateCreated", "DateUpdated", "Description", "Price", "PriceNonRALColour", "ProductId", "UpdatedBy" },
+                values: new object[] { 1, 1, new DateTime(2023, 1, 1, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Markilux Infrared Heater 2500 watt Dimmable", 1393m, 1635m, 6, 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Heaters");
+        }
+    }
+}
---
+++ Model/Products/Heaters.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwningsAPI.Model.Products
+{
+    public class Heaters
+    {
+        [Key]
+        public int HeaterId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceNonRALColour { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int UpdatedBy { get; set; }
+
+        //Navigation Property
+        public int ProductId { get; set; }
+    }
+}
