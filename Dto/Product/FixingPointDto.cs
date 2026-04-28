c4cc48286cc91441434806b6e5d062a4ac66ca0d
Ranjith <ranji_b4u@yahoo.co.in>
2026-04-28 20:52:46 +0100

    Add EmailFunction in git

--- Dto/Product/FixingPointDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AwningsAPI.Dto.Product
-{
-    public class FixingPointDto
-    {
-        public int FixingPointId { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-    }
-}
