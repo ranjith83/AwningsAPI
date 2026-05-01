eddad75b106c72f027678c4a8f1144a7788717a5
Ranjith <ranji_b4u@yahoo.co.in>
2026-05-01 16:41:28 +0100

    Adding DB migration

--- AwningsAPI/Program.cs
+++ AwningsAPI/Program.cs
@@ -138,6 +138,13 @@ builder.Services.AddCors(options =>
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseCors("AllowAngularDev");
 app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
